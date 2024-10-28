6a0630e5d06aa9e56b3a5c4fef8ee53e4deb47a0
Glenn-Lawrence <glenn@splat3d.com>
2024-10-28 20:27:41 +1100

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,13 @@
+input.onButtonPressed(Button.A, function () {
+	
+})
+basic.showLeds(`
+    . . . . .
+    . . . . .
+    . . # . .
+    . . . . .
+    . . . . .
+    `)
+basic.forever(function () {
+	
+})
